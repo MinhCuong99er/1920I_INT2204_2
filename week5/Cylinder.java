bf0dff5858db6582c724fdb8d0b4375bc2308e91
Giáo Sư Đầy Bụng <37433279+MinhCuong99er@users.noreply.github.com>
2019-10-07 11:17:15 +0700

    Update Cylinder.java

--- week5/Cylinder.java
+++ week5/Cylinder.java
@@ -3,25 +3,33 @@ package com.week5;
 public class Cylinder extends Circle{
     private double height = 1.0;
     public Cylinder(){
-
     }
-    public Cylinder(double radius) {
-
+    public Cylinder(double _radius) {
+        super(_radius);
     }
-    public Cylinder(double radius, double height) {
-
+    public Cylinder(double _radius, double _height) {
+        super(_radius);
+        this.height = _height;
     }
-    public Cylinder(double radius, double height, String colour) {
-
+    public Cylinder(double _radius, double _height, String colour) {
+        super(_radius, colour);
+        this.height = _height;
     }
 
     public double getHeight() {
         return height;
     }
-    public void setHeight (doub1le _height){
+    public void setHeight (double _height){
         this.height = _height;
     }
     public double getVolume() {
-
+        return super.getArea() * this.height;
+    }
+    public String toString() {
+        return "Cylinder[" + super.toString() + ",height=" + this.height +"]";
+    }
+    public double getArea() {
+        return super.getArea() * 2 + 2 * this.PI * super.getRadius() * this.height;
     }
 }
+
