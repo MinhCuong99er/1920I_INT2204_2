1b368db562a97bdcf4b3fc2f9685a28fade1b861
Giáo Sư Đầy Bụng <37433279+MinhCuong99er@users.noreply.github.com>
2019-11-11 09:58:47 +0700

    Add files via upload

---
+++ Test/src/com/banchu/GUI.java
@@ -0,0 +1,16 @@
+package com.banchu;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    GUI() {
+        setSize(400, 600);
+ //       setLocation(400, 300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setLayout(null);
+        PlayPanel playPanel = new PlayPanel();
+        add(playPanel);
+    }
+}
